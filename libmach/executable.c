36163abe7d53b3ed664005f36ba395d44f4d9c5f
pad <pad@fb.com>
2014-11-08 23:01:48 -0800

    * libmach/macho.h: start support for mach-O

--- libmach/executable.c
+++ libmach/executable.c
@@ -2,7 +2,10 @@
 #include	<bio.h>
 #include	"bootexec.h"
 #include	"mach.h"
+
 #include	"elf.h"
+typedef u32int uint32;
+#include	"macho.h"
 
 /*
  *	All a.out header types.  The dummy entry allows canonical
@@ -13,20 +16,13 @@ typedef struct {
 	union{
 		Exec exec;			/* in a.out.h */
 		Ehdr ehdr;			/* in elf.h */
-		struct mipsexec mipsexec;
-		struct mips4kexec mips4kexec;
-		struct sparcexec sparcexec;
-		struct nextexec nextexec;
+		Machhdr machhdr;	/* macho.h */
 	} e;
 	long dummy;		/* padding to ensure extra long */
 } ExecHdr;
 
-static	int	nextboot(int, Fhdr*, ExecHdr*);
-static	int	sparcboot(int, Fhdr*, ExecHdr*);
-static	int	mipsboot(int, Fhdr*, ExecHdr*);
-static	int	mips4kboot(int, Fhdr*, ExecHdr*);
 static	int	common(int, Fhdr*, ExecHdr*);
-static	int	adotout(int, Fhdr*, ExecHdr*);
+static	int	machdotout(int, Fhdr*, ExecHdr*);
 static	int	elfdotout(int, Fhdr*, ExecHdr*);
 static	int	armdotout(int, Fhdr*, ExecHdr*);
 static	void	setsym(Fhdr*, long, long, long, long);
@@ -49,18 +45,11 @@ typedef struct Exectable{
 	int	(*hparse)(int, Fhdr*, ExecHdr*);
 } ExecTable;
 
-extern	Mach	mmips;
 extern	Mach	mi386;
+extern	Mach	marm;
 
 ExecTable exectab[] =
 {
-	{ V_MAGIC,			/* Mips v.out */
-		"mips plan 9 executable",
-		FMIPS,
-		&mmips,
-		sizeof(Exec),
-		beswal,
-		adotout },
 	{ I_MAGIC,			/* I386 8.out & boot image */
 		"386 plan 9 executable",
 		FI386,
@@ -68,32 +57,33 @@ ExecTable exectab[] =
 		sizeof(Exec),
 		beswal,
 		common },
+	{ E_MAGIC,			/* Arm 5.out */
+		"Arm plan 9 executable",
+		FARM,
+		&marm,
+		sizeof(Exec),
+		beswal,
+		common },
+//	{ ELF_MAG,			/* any elf32 or elf64 */
+//		"elf executable",
+//		nil,
+//		FNONE,
+//		0,
+//		&mi386,
+//		sizeof(Ehdr64),
+//		nil,
+//		elfdotout },
+	{ MACH32_MAG,			/* 32-bit MACH (apple mac) */
+		"mach executable",
+		FI386,
+		&mi386,
+		sizeof(Machhdr),
+		leswal, // used to be nil
+		machdotout },
 	{ 0 },
 };
 
-Mach	*mach = &mmips;			/* Global current machine table */
-
-ExecTable*
-couldbe4k(ExecTable *mp)
-{
-	Dir *d;
-	ExecTable *f;
-
-	if((d=dirstat("/proc/1/regs")) == nil)
-		return mp;
-	if(d->length < 32*8){		/* R3000 */
-		free(d);
-		return mp;
-	}
-	free(d);
-	for (f = exectab; f->magic; f++)
-		if(f->magic == M_MAGIC) {
-			f->name = "mips plan 9 executable on mips2 kernel";
-			return f;
-		}
-	return mp;
-}
-
+Mach	*mach = &mi386;			/* Global current machine table */
 
 int
 crackhdr(int fd, Fhdr *fp)
@@ -111,8 +101,8 @@ crackhdr(int fd, Fhdr *fp)
 	fp->magic = magic = beswal(d.e.exec.magic);		/* big-endian */
 	for (mp = exectab; mp->magic; mp++) {
 		if (mp->magic == magic && nb >= mp->hsize) {
-			if(mp->magic == V_MAGIC)
-				mp = couldbe4k(mp);
+			//if(mp->magic == V_MAGIC)
+			//	mp = couldbe4k(mp);
 
 			hswal((long *) &d, sizeof(d.e)/sizeof(long), mp->swal);
 			fp->type = mp->type;
@@ -211,96 +201,6 @@ common(int fd, Fhdr *fp, ExecHdr *hp)
 	return 1;
 }
 
-/*
- *	mips bootable image.
- */
-static int
-mipsboot(int fd, Fhdr *fp, ExecHdr *hp)
-{
-	USED(fd);
-	switch(hp->e.mipsexec.amagic) {
-	default:
-	case 0407:	/* some kind of mips */
-		fp->type = FMIPSB;
-		settext(fp, hp->e.mipsexec.mentry, hp->e.mipsexec.text_start, hp->e.mipsexec.tsize,
-					sizeof(struct mipsexec)+4);
-		setdata(fp, hp->e.mipsexec.data_start, hp->e.mipsexec.dsize,
-				fp->txtoff+hp->e.mipsexec.tsize, hp->e.mipsexec.bsize);
-		break;
-	case 0413:	/* some kind of mips */
-		fp->type = FMIPSB;
-		settext(fp, hp->e.mipsexec.mentry, hp->e.mipsexec.text_start, hp->e.mipsexec.tsize, 0);
-		setdata(fp, hp->e.mipsexec.data_start, hp->e.mipsexec.dsize, hp->e.mipsexec.tsize,
-					hp->e.mipsexec.bsize);
-		break;
-	}
-	setsym(fp, hp->e.mipsexec.nsyms, 0, hp->e.mipsexec.u0.mpcsize, hp->e.mipsexec.symptr);
-	fp->hdrsz = 0;		/* header stripped */
-	return 1;
-}
-
-/*
- *	mips4k bootable image.
- */
-static int
-mips4kboot(int fd, Fhdr *fp, ExecHdr *hp)
-{
-	USED(fd);
-	switch(hp->e.mipsexec.amagic) {
-	default:
-	case 0407:	/* some kind of mips */
-		fp->type = FMIPSB;
-		settext(fp, hp->e.mipsexec.mentry, hp->e.mipsexec.text_start, hp->e.mipsexec.tsize,
-					sizeof(struct mips4kexec));
-		setdata(fp, hp->e.mipsexec.data_start, hp->e.mipsexec.dsize,
-				fp->txtoff+hp->e.mipsexec.tsize, hp->e.mipsexec.bsize);
-		break;
-	case 0413:	/* some kind of mips */
-		fp->type = FMIPSB;
-		settext(fp, hp->e.mipsexec.mentry, hp->e.mipsexec.text_start, hp->e.mipsexec.tsize, 0);
-		setdata(fp, hp->e.mipsexec.data_start, hp->e.mipsexec.dsize, hp->e.mipsexec.tsize,
-					hp->e.mipsexec.bsize);
-		break;
-	}
-	setsym(fp, hp->e.mipsexec.nsyms, 0, hp->e.mipsexec.u0.mpcsize, hp->e.mipsexec.symptr);
-	fp->hdrsz = 0;		/* header stripped */
-	return 1;
-}
-
-/*
- *	sparc bootable image
- */
-static int
-sparcboot(int fd, Fhdr *fp, ExecHdr *hp)
-{
-	USED(fd);
-	fp->type = FSPARCB;
-	settext(fp, hp->e.sparcexec.sentry, hp->e.sparcexec.sentry, hp->e.sparcexec.stext,
-					sizeof(struct sparcexec));
-	setdata(fp, hp->e.sparcexec.sentry+hp->e.sparcexec.stext, hp->e.sparcexec.sdata,
-					fp->txtoff+hp->e.sparcexec.stext, hp->e.sparcexec.sbss);
-	setsym(fp, hp->e.sparcexec.ssyms, 0, hp->e.sparcexec.sdrsize, fp->datoff+hp->e.sparcexec.sdata);
-	fp->hdrsz = 0;		/* header stripped */
-	return 1;
-}
-
-/*
- *	next bootable image
- */
-static int
-nextboot(int fd, Fhdr *fp, ExecHdr *hp)
-{
-	USED(fd);
-	fp->type = FNEXTB;
-	settext(fp, hp->e.nextexec.textc.vmaddr, hp->e.nextexec.textc.vmaddr,
-					hp->e.nextexec.texts.size, hp->e.nextexec.texts.offset);
-	setdata(fp, hp->e.nextexec.datac.vmaddr, hp->e.nextexec.datas.size,
-				hp->e.nextexec.datas.offset, hp->e.nextexec.bsss.size);
-	setsym(fp, hp->e.nextexec.symc.nsyms, hp->e.nextexec.symc.spoff, hp->e.nextexec.symc.pcoff,
-					hp->e.nextexec.symc.symoff);
-	fp->hdrsz = 0;		/* header stripped */
-	return 1;
-}
 
 static Shdr*
 elfsectbyname(int fd, Ehdr *hp, Shdr *sp, char *name)
@@ -416,6 +316,236 @@ bad:
 	werrstr("ELF sections scrambled");
 	return 0;
 }
+
+
+static int
+machdotout(int fd, Fhdr *fp, ExecHdr *hp)
+{
+	uvlong (*swav)(uvlong);
+	uint32 (*swal)(uint32);
+	Machhdr *mp;
+	MachCmd **cmd;
+	MachSymSeg *symtab;
+	MachSymSeg *pclntab;
+	MachSeg64 *seg;
+	MachSect64 *sect;
+	MachSeg32 *seg32;
+	MachSect32 *sect32;
+	uvlong textsize, datasize, bsssize;
+	uchar *cmdbuf;
+	uchar *cmdp;
+	int i, j, hdrsize;
+	uint32 textva, textoff, datava, dataoff, symoff, symsize, pclnoff, pclnsize;
+
+	mp = &hp->e.machhdr;
+	if (leswal(mp->filetype) != MACH_EXECUTABLE_TYPE) {
+		werrstr("bad MACH executable type %#ux", leswal(mp->filetype));
+		return 0;
+	}
+
+	swal = leswal;
+	swav = leswav;
+
+	mp->magic = swal(mp->magic);
+	mp->cputype = swal(mp->cputype);
+	mp->cpusubtype = swal(mp->cpusubtype);
+	mp->filetype = swal(mp->filetype);
+	mp->ncmds = swal(mp->ncmds);
+	mp->sizeofcmds = swal(mp->sizeofcmds);
+	mp->flags = swal(mp->flags);
+	mp->reserved = swal(mp->reserved);
+
+	switch(mp->magic) {
+	case 0xFEEDFACE:	// 32-bit mach
+		if (mp->cputype != MACH_CPU_TYPE_X86) {
+			werrstr("bad MACH cpu type - not 386");
+			return 0;
+		}
+		if (mp->cpusubtype != MACH_CPU_SUBTYPE_X86) {
+			werrstr("bad MACH cpu subtype - not 386");
+			return 0;
+		}
+		if (mp->filetype != MACH_EXECUTABLE_TYPE) {
+			werrstr("bad MACH executable type");
+			return 0;
+		}
+		mach = &mi386;
+		fp->type = FI386;
+		hdrsize = 28;
+		break;
+
+	case 0xFEEDFACF:	// 64-bit mach
+		werrstr("64 bit mach NOT SUPPORTED");
+		return 0;
+		//if (mp->cputype != MACH_CPU_TYPE_X86_64) {
+		//	werrstr("bad MACH cpu type - not amd64");
+		//	return 0;
+		//}
+        //
+		//if (mp->cpusubtype != MACH_CPU_SUBTYPE_X86 && mp->cpusubtype != MACH_CPU_SUBTYPE_X86_64) {
+		//	werrstr("bad MACH cpu subtype - not amd64");
+		//	return 0;
+		//}
+		//mach = &mamd64;
+		//fp->type = FAMD64;
+		//hdrsize = 32;
+		//break;
+
+	default:
+		werrstr("not mach %#ux", mp->magic);
+		return 0;
+	}
+
+	cmdbuf = malloc(mp->sizeofcmds);
+	if(!cmdbuf) {
+		werrstr("out of memory");
+		return 0;
+	}
+	seek(fd, hdrsize, 0);
+	if(read(fd, cmdbuf, mp->sizeofcmds) != mp->sizeofcmds) {
+		free(cmdbuf);
+		return 0;
+	}
+	cmd = malloc(mp->ncmds * sizeof(MachCmd*));
+	if(!cmd) {
+		free(cmdbuf);
+		werrstr("out of memory");
+		return 0;
+	}
+	cmdp = cmdbuf;
+	textva = 0;
+	textoff = 0;
+	dataoff = 0;
+	datava = 0;
+	symtab = 0;
+	pclntab = 0;
+	textsize = 0;
+	datasize = 0;
+	bsssize = 0;
+	symoff = 0;
+	symsize = 0;
+	pclnoff = 0;
+	pclnsize = 0;
+	for (i = 0; i < mp->ncmds; i++) {
+		MachCmd *c;
+
+		cmd[i] = (MachCmd*)cmdp;
+		c = cmd[i];
+		c->type = swal(c->type);
+		c->size = swal(c->size);
+		switch(c->type) {
+		case MACH_SEGMENT_32:
+			if(mp->magic != 0xFEEDFACE) {
+				werrstr("segment 32 in mach 64");
+				goto bad;
+			}
+			seg32 = (MachSeg32*)c;
+			seg32->vmaddr = swav(seg32->vmaddr);
+			seg32->vmsize = swav(seg32->vmsize);
+			seg32->fileoff = swav(seg32->fileoff);
+			seg32->filesize = swav(seg32->filesize);
+			seg32->maxprot = swal(seg32->maxprot);
+			seg32->initprot = swal(seg32->initprot);
+			seg32->nsects = swal(seg32->nsects);
+			seg32->flags = swal(seg32->flags);
+			if (strcmp(seg32->segname, "__TEXT") == 0) {
+				textva = seg32->vmaddr;
+				textoff = seg32->fileoff;
+				textsize = seg32->vmsize;
+				sect32 = (MachSect32*)(cmdp + sizeof(MachSeg32));
+				for(j = 0; j < seg32->nsects; j++, sect32++) {
+					if (strcmp(sect32->sectname, "__gosymtab") == 0) {
+						symoff = swal(sect32->offset);
+						symsize = swal(sect32->size);
+					}
+					if (strcmp(sect32->sectname, "__gopclntab") == 0) {
+						pclnoff = swal(sect32->offset);
+						pclnsize = swal(sect32->size);
+					}
+				}
+			}
+			if (strcmp(seg32->segname, "__DATA") == 0) {
+				datava = seg32->vmaddr;
+				dataoff = seg32->fileoff;
+				datasize = seg32->filesize;
+				bsssize = seg32->vmsize - seg32->filesize;
+			}
+			break;
+
+		case MACH_SEGMENT_64:
+			werrstr("MACH SEGMENT 64 NOT SUPPORTED");
+				goto bad;
+			//if(mp->magic != 0xFEEDFACF) {
+			//	werrstr("segment 32 in mach 64");
+			//	goto bad;
+			//}
+			//seg = (MachSeg64*)c;
+			//seg->vmaddr = swav(seg->vmaddr);
+			//seg->vmsize = swav(seg->vmsize);
+			//seg->fileoff = swav(seg->fileoff);
+			//seg->filesize = swav(seg->filesize);
+			//seg->maxprot = swal(seg->maxprot);
+			//seg->initprot = swal(seg->initprot);
+			//seg->nsects = swal(seg->nsects);
+			//seg->flags = swal(seg->flags);
+			//if (strcmp(seg->segname, "__TEXT") == 0) {
+			//	textva = seg->vmaddr;
+			//	textoff = seg->fileoff;
+			//	textsize = seg->vmsize;
+			//	sect = (MachSect64*)(cmdp + sizeof(MachSeg64));
+			//	for(j = 0; j < seg->nsects; j++, sect++) {
+			//		if (strcmp(sect->sectname, "__gosymtab") == 0) {
+			//			symoff = swal(sect->offset);
+			//			symsize = swal(sect->size);
+			//		}
+			//		if (strcmp(sect->sectname, "__gopclntab") == 0) {
+			//			pclnoff = swal(sect->offset);
+			//			pclnsize = swal(sect->size);
+			//		}
+			//	}
+			//}
+			//if (strcmp(seg->segname, "__DATA") == 0) {
+			//	datava = seg->vmaddr;
+			//	dataoff = seg->fileoff;
+			//	datasize = seg->filesize;
+			//	bsssize = seg->vmsize - seg->filesize;
+			//}
+			break;
+		case MACH_UNIXTHREAD:
+			break;
+		case MACH_SYMSEG:
+			if (symtab == 0) {
+				symtab = (MachSymSeg*)c;
+				symoff = swal(symtab->fileoff);
+				symsize = swal(symtab->filesize);
+			} else if (pclntab == 0) {
+				pclntab = (MachSymSeg*)c;
+				pclnoff = swal(pclntab->fileoff);
+				pclnsize = swal(pclntab->filesize);
+			}
+			break;
+		}
+		cmdp += c->size;
+	}
+	if (textva == 0 || datava == 0) {
+		free(cmd);
+		free(cmdbuf);
+		return 0;
+	}
+	/* compute entry by taking address after header - weird - BUG? */
+	settext(fp, textva+sizeof(Machhdr) + mp->sizeofcmds, textva, textsize, textoff);
+	setdata(fp, datava, datasize, dataoff, bsssize);
+	if(symoff > 0)
+		setsym(fp, symoff, symsize, 0, 0);//TODO, pclnoff, pclnsize);
+	free(cmd);
+	free(cmdbuf);
+	return 1;
+bad:
+	free(cmd);
+	free(cmdbuf);
+	return 0;
+}
+
 
 /*
  * (Free|Net)BSD ARM header.
